c011ba4bfaeb15069116773ce7296e767f2d11c5
bartvanbenthem <bartvanbenthem@hotmail.com>
2025-01-17 14:46:20 +0100

    update run w sas token gen

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
+# BUILD DISPATCHER
 cargo fmt
 cargo build --release
 
-export IMAGE_NAME='dummy-image-bvb'
-export IMAGE_PATH=$(echo https://azlimgdistribute.blob.core.windows.net/images/$IMAGE_NAME.vhd)
+# GENERATE SAS TOKEN THAT IS VALID FOR 2 HOURS
+TOKEN_EXP=$(date -u -d '+2 hours' +"%Y-%m-%dT%H:%M:%SZ")
+SAS_TOKEN=$(az storage container generate-sas --account-name azlimgdistribute --name images --permissions r --expiry $TOKEN_EXP --https-only --output tsv)
+clear
+
+echo ""
+echo "Staring Image Distribution"
+echo "--------------------------"
+
+# CONSTRUCT IMAGE URL
+export IMAGE_NAME='rhel9-basic-v1'
+export IMAGE_PATH=$(echo https://azlimgdistribute.blob.core.windows.net/images/$IMAGE_NAME.vhdx?$SAS_TOKEN)
+
 export SUBSCRIPTION='d38b5566-1cb7-411a-95ac-e94507237470'
 export LOCATION='westeurope'
 export OS_TYPE='Linux'
